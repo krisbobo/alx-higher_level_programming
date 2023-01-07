7841b2601549c982db3c0add0431684df371898f
krisbobo <kiyongbohbo@gmail.com>
2023-01-06 19:12:28 -0800

    Update file

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""
-Program that Ident a text according to '.' and '?' symbol
-"""
+# 5-text_indentation.py
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """  function that prints a text with 2 new lines after
-    each of these characters: ., ?
+    """Print text with two new lines after each '.', '?', and ':'.
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
     """
-    if (type(text) is not str or text is None):
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    flag = 0
-    for c in text:
-        if (c == '.' or c == '?' or c == ':'):
-            print(c, end='')
-            print('')
-            print('')
-            flag = 1
-        else:
-            if (flag == 0):
-                print(c, end='')
-            else:
-                if (c == ' ' or c == '\t'):
-                    pass
-                else:
-                    print(c, end="")
-                    flag = 0
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1

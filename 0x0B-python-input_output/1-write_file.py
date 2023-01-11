a45d27a43c22e46920030f66d144cdbab04bf0c4
krisbobo <kiyongbohbo@gmail.com>
2023-01-10 16:52:46 -0800

    Update file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-""" Program that Read n lines """
+# 1-number_of_lines.py
+
+"""Defines a text file line-counting function."""
 
 
-def read_lines(filename="", nb_lines=0):
-    """ function that reads n lines of a text file (UTF8) and
-    prints it to stdout """
-    count = 0
-    with open(filename, encoding='utf-8') as f:
-        if (nb_lines <= 0):
-            print(f.read(), end='')
+def number_of_lines(filename=""):
+    """Return the number of lines in a text file."""
+    lines = 0
+    with open(filename) as f:
         for line in f:
-            if (count < nb_lines):
-                print(line, end='')
-                count += 1
+            lines += 1
+    return lines
